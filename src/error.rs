f6b6030ee95e3c6b8f09c959f11976ed001a8a4a
Emilia Hane <emilia.hane@ethereum.org>
2023-04-26 13:25:34 +0200

    fixup! Use thiserror

--- src/error.rs
+++ src/error.rs
@@ -4,12 +4,12 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum HolePunchError<Discv5Error: Debug + Display> {
-    #[error("error parsing notification")]
+    #[error("error parsing notification, {0}")]
     NotificationError(#[from] DecoderError),
-    #[error("failed initiating a hole punch attempt")]
+    #[error("failed initiating a hole punch attempt, {0}")]
     InitiatorError(Discv5Error),
-    #[error("failed relaying a hole punch attempt")]
+    #[error("failed relaying a hole punch attempt, {0}")]
     RelayError(Discv5Error),
-    #[error("failed as target of a hole punch attempt")]
+    #[error("failed as target of a hole punch attempt, {0}")]
     TargetError(Discv5Error),
 }
